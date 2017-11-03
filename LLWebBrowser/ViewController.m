3d1efe79097b1f629cd6fe15056334a0ecca783c
WangZhaomeng <wangzhaomeng@wangzhaomengdemacbook-pro.local>
2017-11-03 10:36:03 +0800

    add

--- LLWebBrowser/ViewController.m
+++ LLWebBrowser/ViewController.m
@@ -18,13 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSArray *titles = @[@"百度",@"html",@"百度",@"html"];
+    NSArray *titles = @[@"根据iOS版本，选择合适的webview加载：百度",
+                        @"根据iOS版本，选择合适的webview加载：html",
+                        @"强制使用UIWebView加载：百度",
+                        @"强制使用UIWebView加载：html"];
     for (NSInteger i = 0; i < 4; i ++) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
         btn.tag = i;
-        btn.frame = CGRectMake(20, 100+i*50, 60, 30);
-        btn.backgroundColor = [UIColor grayColor];
+        btn.frame = CGRectMake(0, 100+i*50, [UIScreen mainScreen].bounds.size.width, 30);
+        btn.titleLabel.font = [UIFont systemFontOfSize:12];
         [btn setTitle:titles[i] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
         [btn addTarget:self action:@selector(gotoWebBrowser:) forControlEvents:UIControlEventTouchUpInside];
         [self.view addSubview:btn];
     }
@@ -35,8 +39,16 @@
     if (btn.tag == 0) {
         webVC = [[LLWebViewController alloc] initWithUrl:@"http://www.baidu.com/"];
     }
-    else{
+    else if (btn.tag == 1) {
         webVC = [[LLWebViewController alloc] initWithHtml:@"LLTest"];
+    }
+    else if (btn.tag == 2) {
+        webVC = [[LLWebViewController alloc] initWithUrl:@"http://www.baidu.com/"];
+        webVC.loadType = LLWebViewLoadTypeUIWebView;
+    }
+    else {
+        webVC = [[LLWebViewController alloc] initWithHtml:@"LLTest"];
+        webVC.loadType = LLWebViewLoadTypeUIWebView;
     }
     [self.navigationController pushViewController:webVC animated:YES];
 }
